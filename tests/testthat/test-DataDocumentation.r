a95b8f2c927acc4743e8dd9a74eb1ce008a280f6
vagdur <vilhelm.agdur@gmail.com>
2021-02-21 02:07:10 +0100

    Expand documentation testing
    Added more tests of the documentation files -- it now tests that the file it claims to document does exist.
    
    Also improved control flow of the testing a little bit, for clearer failure messages.

--- tests/testthat/test-DataDocumentation.r
+++ tests/testthat/test-DataDocumentation.r
@@ -29,11 +29,76 @@ checkFormatConformity <- function(filename) {
   parsedXML <- xml2::read_xml(filename)
   xmlSchema <- xml2::read_xml(system.file("extdata", "TableDocumentationFormat.xsd", package = "SCBHandlerPlotter"))
 
+  # Now, let's test that the XML matches the Schema:
+  test_that(paste("file ",fileShortName," conforms to the XML Schema"), {
+    expect_true(xml2::xml_validate(parsedXML, xmlSchema))
+  })
+
+  # If the XML fails to conform to our schema, it makes no sense to continue testing, so we return FALSE to indicate
+  # the tests failed: (We need to do this since the caller of the function needs to know whether to continue testing.)
+  if (!xml2::xml_validate(parsedXML, xmlSchema)) {
+    return(FALSE)
+  }
+
+
+  # Now the easy part of our testing is done -- the file does exist, and it is a proper XML file which conforms to
+  # our XML Schema. It remains to test the things that cannot be checked just using a context-free grammar: That the
+  # column specifications actually make sense for a table, and names are unambiguous.
+
   # We need a return value here, so that the code executing this function can itself be a test. Otherwise the testing
   # will abort as soon as we get an error in here, and so not all XML files would get tested as soon as one fails. Thus:
   return(TRUE)
 }
 
+
+# Check that the documentation of the XML file matches the actual data
+#
+# This function actually does two things: First it checks that the file in question conforms to the format for a data
+# documentation file, using the function checkFormatConformity, and then (assuming the file it got can indeed be the
+# the documentation of a table) it checks that the documentation is correct.
+#
+# To do this, it checks in order:
+#   1. That the file it claims to be documenting exists
+#   2. That the file it claims to be documenting is indeed a csv
+#   3. That the csv has exactly the columns the documentation claims it has
+#   4. That each column's levels are correctly specified:
+#       a)  A column with levelsType set to Municipalities contains only names of municipalities.
+#       b)  A column with levelsType set to NumericRange is numeric, and if an upper or lower bound is specified,
+#           then all values fall above (below) the lower (upper) bound.
+#       c)  A column with levelsType set to Character contains only elements as specified by one of the <level> tags.
+#   5. That the valueColumn is numeric, possibly with NAs.
+
+checkDocumentationCorrectness <- function(filename) {
+  # To begin with, we check that it makes sense to check documentation correctness -- that is, that the file really
+  # is a potentially correct documentation file:
+  if(!checkFormatConformity(filename)) {
+    # If this failed, it makes no sense to continue, so we return FALSE:
+    return(FALSE)
+  }
+
+  # For prettier output from our tests, let's just write the name of the file, not its whole path:
+  fileShortName <- basename(filename)
+
+  # Knowing that the XML makes sense, we read it in and cast it to a super-nested list to work with:
+  parsedXML <- xml2::read_xml(filename)
+  docList <- xml2::as_list(parsedXML)$table
+
+  # The first thing to do is to find the csv file it says it documents and test that it exists:
+  csvFilename <- docList$filename[[1]][1] # The [[1]][1] is for some reason how the list we get is structured?
+  test_that(paste("file ",csvFilename," which ",fileShortName," claims to document exists"),{
+    expect_true(file.exists(system.file("extdata",csvFilename, package="SCBHandlerPlotter")))
+  })
+  if (!file.exists(system.file("extdata",csvFilename, package="SCBHandlerPlotter"))) {
+    # If it doesn't exist, we can't continue:
+    return(FALSE)
+  }
+  # If it does exist, we can read it in:
+  dataTable <- read.csv(system.file("extdata",csvFilename, package="SCBHandlerPlotter"))
+
+  # We need a return value here, so that the code executing this function can itself be a test. Otherwise the testing
+  # will abort as soon as we get an error in here, and so not all XML files would get tested as soon as one fails. Thus:
+  return(TRUE)
+}
 
 # Our very first test should be that we can actually read in the XML Schema we need to validate our XML files:
 test_that("XML Schema can be read",{
@@ -48,7 +113,7 @@ tablesToCheck <- list.files(extdataLocation, pattern="*\\.xml$")
 for (tableToCheck in tablesToCheck) {
   # We wrap this in a test so that the loop won't stop just because one file fails its tests.
   # If this test fails, there'll also be more granular tests that have failed:
-  test_that(paste("file ",tableToCheck," is in the proper format for a documentation file"),{
-    expect_true(checkFormatConformity(paste(extdataLocation,"/",tableToCheck,sep="")))
+  test_that(paste("documentation ",tableToCheck," is correct"),{
+    expect_true(checkDocumentationCorrectness(paste(extdataLocation,"/",tableToCheck,sep="")))
   })
 }
