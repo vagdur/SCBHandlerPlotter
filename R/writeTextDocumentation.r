92671d2aafd001a74e792b7006b7eb1906a2a2d5
vagdur <vilhelm.agdur@gmail.com>
2021-02-24 18:17:08 +0100

    Finished writeTextDocumentation
    The function now writes out a correctly formatted (with the right encoding -- that was the most difficult part) .Rd document documenting the XML table it was fed, as a string.
    
    The next step is to create a suitable script to run this function on each XML file, before build time of the package.

--- R/writeTextDocumentation.r
+++ R/writeTextDocumentation.r
@@ -13,6 +13,95 @@
 writeTextDocumentation <- function(filename) {
   # The documentation can be built incrementally by just appending stuff to a string:
   doc <- ""
+  # We are always documenting data, and may include some non-ASCII characters, so:
+  doc <- paste(doc,'\\docType{data}\n\\encoding{utf8}\n',sep="")
+
+  # We load the XML documentation file and turn it into a list: (Note that we assume we are given its entire path,
+  # not just its filename. We also assume that the XML documentation has been tested.)
+  parsedXML <- xml2::read_xml(filename, encoding="UTF-8")
+  docList <- xml2::as_list(parsedXML)$table
+
+  # We start by writing the name anb title of the dataset:
+  tableName <- docList$name[[1]][1]
+  tableTitle <- docList$title[[1]][1]
+  doc <- paste(doc,'\\name{',tableName,'}\n\\alias{',tableName,'}\n\\title{',tableTitle,'}\n',sep="")
+
+  # Now we proceed to write the documentation of the columns that are present:
+  tableColumns <- docList$columns
+  doc <- paste(doc,'\\format{A table with ',length(tableColumns)-1,' variables, to be queried with the SCB() function.\n\\describe{\n',sep="")
+  for (columnIndex in 1:(length(tableColumns)-1)) { # -1 because we don't want to document the valueColumn
+    currentColumn <- tableColumns[[columnIndex]]
+    curColIdentifier <- currentColumn$identifier
+    # Start writing the documentation of this column:
+    doc <- paste(doc,"\\item{",curColIdentifier,"}{",sep="")
+    # First, we figure out if this column has any aliases, and if so, we write them:
+    if (length(currentColumn) > 3) { # There's always <identifier>, <colname>, and <levels>, so any additional tags must be <alias>
+      columnAliases <- sapply(c(3:length(currentColumn)-1), function(x) currentColumn[[x]][[1]][1])
+      doc <- paste(doc,"Also known as: ",paste(columnAliases, collapse=", "),".\n",sep="")
+    }
+    # What we write next depends on the type of levels the column has:
+    columnLevelsType <- currentColumn$levels$levelsType[[1]][1]
+    if (columnLevelsType == "Municipalities") {
+      # This is the easiest case, since now the levelsType has told us everything we need to know about the column.
+      doc <- paste(doc, "Municipality the observation is from.", sep="")
+    } else if (columnLevelsType == "NumericRange") {
+      # This is also a relatively simple case, all we need to do is figure out if a maxLevel and/or minLevel has been specified
+      doc <- paste(doc,"A range of numeric values",sep="")
+      # So there are four cases depending on which if any of maxLevel and minLevel are set
+      if (!is.null(currentColumn$levels$minLevel)) {
+        if (!is.null(currentColumn$levels$maxLevel)) {
+          # Both maxLevel and minLevel are set:
+          doc <- paste(doc,", taking values between ",currentColumn$levels$minLevel," and ",currentColumn$levels$maxLevel,".", sep="")
+        } else {
+          # minLevel is set, but maxLevel is not:
+          doc <- paste(doc,", taking values above ",currentColumn$levels$minLevel,".",sep="")
+        }
+      } else {
+        if (!is.null(currentColumn$levels$maxLevel)) {
+          # minLevel is not set, but maxLevel is:
+          doc <- paste(doc,", taking values below ",currentColumn$levels$maxLevel,".",sep="")
+        } else {
+          # Neither maxLevel nor minLevel are set:
+          # There is nothing more to add, so we just add a period at the end of the sentence.
+          doc <- paste(doc,".",sep="")
+        }
+      }
+    } else if (columnLevelsType == "Character") {
+      # In this case, we need to do a bit of work, looping through all the specified levels of the table, and
+      # printing their name and aliases. Getting this to look good with commas in the list and so on gets a bit messy...
+      doc <- paste(doc,"A column of factor data. Its levels are: ",sep="")
+      columnLevels <- currentColumn$levels
+      # To make the commas in the list display right, we first create a list of what should be said about each level,
+      # and then paste that list together:
+      columnLevelsDocs <- character(0)
+      for (levelIndex in 2:(length(columnLevels))) {
+        currentLevel <- columnLevels[[levelIndex]]
+        # We create a string that should contain what is to be said about this level:
+        currentLevelDoc <- currentLevel$levelName
+        if (length(currentLevel) > 1) { # i.e. there are some aliases, which we note inside parentheses:
+          currentLevelDoc <- paste(currentLevelDoc, " (a.k.a.: ",sep="")
+          levelAliases <- sapply(c(2:length(currentLevel)), function(x) currentLevel[[x]][[1]][1])
+          currentLevelDoc <- paste(currentLevelDoc, paste(levelAliases, collapse = ", "), ".)",sep="")
+        }
+        columnLevelsDocs <- append(columnLevelsDocs,currentLevelDoc)
+      }
+      # Now we have our list of what is to be said about each level, so we write it in our documentation:
+      doc <- paste(doc,paste(columnLevelsDocs, collapse=", "),".",sep="")
+    } else {
+      stop("Tried to document a column with invalid levelsType. Are you sure the documentation XML passed all tests?")
+    }
+
+    # We close the final bracket in our \item{}{ block:
+    doc <- paste(doc,"}\n",sep="")
+  }
+
+  # We close the opening { of the \format{ and \describe{ tags:
+  doc <- paste(doc,'}}\n',sep="")
+
+  # The final thing we need to add is the description and keyword 'datasets': (Things don't appear in this order when rendered, but this is
+  # the order Roxygen would put them, so we follow their ordering.)
+  tableDescription <- docList$description[[1]][1]
+  doc <- paste(doc, '\\description{',tableDescription,'}\n\\keywords{datasets}',sep="")
 
   # Having created all the documentation, we now just return the string:
   return(doc)
