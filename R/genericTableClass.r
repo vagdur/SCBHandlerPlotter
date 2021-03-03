fdc16fdb15e320dc2065dd1b59f77558dbff95af
vagdur <vilhelm.agdur@gmail.com>
2021-03-04 00:37:17 +0100

    Began creating class Column
    The class has been defined, along with its getters and setters, and tests of the class have been written.
    
    What remains to do is to define the levelsDealias function, which is really the raison d'etre for the class: The possibility of passing an alias for a level of the column to the column object and automatically getting the name it has in the csv file back.
    
    Already realising this code will need to be refactored to use inheritance instead of an internal flag, but since I don't yet know how to do inheritance in R OOP, that will have to wait until I learn how to do it. The current code works, it's just a little bit stupid.

--- R/genericTableClass.r
+++ R/genericTableClass.r
@@ -56,6 +56,7 @@ setValidity("Level", function(object) {
   if (length(object@name) != 1) {
     return("Name of level should be a single string, not a vector")
   }
+  return(TRUE)
 })
 # Define setter and getter for name and aliases:
 setMethod("name", "Level", function(x) x@name)
@@ -83,6 +84,202 @@ setMethod("dealias","Level", function(x, toDealias) {
       x@name
     } else {
       NULL
+    }
+  }))))
+})
+
+####################################################################################
+# Definition of the Column class:
+
+# Create the class:
+setClass("Column",
+         slots = c(
+           name = "character",
+           aliases = "character",
+           colLevels = "list",
+           levelsType = "character",
+           maxLevel = "numeric",
+           minLevel = "numeric"
+         ),
+         prototype = c(
+           name = NA_character_,
+           aliases = NA_character_,
+           colLevels = list(),        # This is named colLevels instead of just levels because "levels" is reserved by the language.
+           levelsType = NA_character_,
+           maxLevel = NA_real_,
+           minLevel = NA_real_
+         ))
+
+# Define a constructor:
+# Note that this code does absolutely no checking of object validity. That is
+# done by the validator, which is called automatically by new()
+Column <- function(name, aliases=NA_character_, levelsType, colLevels=list(), maxLevel=NA_real_, minLevel=NA_real_) {
+  new("Column", name=name, aliases=aliases, levelsType=levelsType, colLevels=colLevels, maxLevel=maxLevel, minLevel=minLevel)
+}
+# This wants to know the name and aliases of the column to be created,
+# and either a list of Level objects, a pair of numbers, or NA, depending on the levelsType.
+# levelsType should be one of "Municipalities", "NumericRange", or "Character", just as in
+# the documentation format.
+setValidity("Column", function(object) {
+  # It should have exactly one name and one levelsType:
+  if (length(object@name) != 1) {
+    return("Name of column should be a single string, not a vector")
+  }
+  if (length(object@levelsType) != 1) {
+    return("levelsType of column should be a single string, not a vector")
+  } else if (!(object@levelsType %in% c("Municipalities", "NumericRange", "Character"))) {
+    return("levelsType of column must be one of Municipalities, NumericRange, or Character.")
+  }
+
+  if (object@levelsType != "Character") {
+    # Only a column with levelsType Character is permitted to have its "colLevels" be not-NA.
+    # If a "colLevels" was nonetheless supplied, we ignore it with a warning:
+    if (!(length(object@colLevels) == 0)) {
+      warning('A "colLevels" was set in Column despite levelsType not being "Character". This will be ignored.')
+    }
+  }
+  if (object@levelsType != "NumericRange") {
+    # Only a column with levelsType NumericRange is permitted to have maxLevel or minLevel set.
+    # If either is nevertheless supplied, we ignore it with a warning:
+    if (!is.na(object@maxLevel)||!is.na(object@minLevel)) {
+      warning("A maxLevel or minLevel was set in Column despite levelsType not being NumericRange. This will be ignored.")
+    }
+  }
+
+  # The preceding checks are actually enough for non-Character columns. For Character columns, we need
+  # some more checks:
+  if (object@levelsType=="Character") {
+    if (length(object@colLevels) == 0) {
+      # A column of levelsType character needs to have colLevels specified:
+      return("A Column of levelsType Character needs to have colLevels specified.")
+    }
+    # Next, we check that each object in colLevels really is a level, and is furthermore
+    # a valid object of that class:
+    for (level in object@colLevels) {
+      if (!is(level)=="Level") {
+        return("List of colLevels should consist of Level objects")
+      }
+      validObject(level)
+    }
+  }
+  # If everything worked, we return TRUE:
+  return(TRUE)
+})
+
+# Define setters and getters for all slots of the Column class: (This entire part is a *huge* DRY violation, but that's just R, I guess?)
+setMethod("name","Column", function(x) x@name)
+setMethod("name<-", "Column", function(x,value) {
+  x@name <- value
+  validObject(x)
+  x
+})
+setMethod("aliases", "Column", function(x) x@aliases)
+setMethod("aliases<-","Column", function(x, value) {
+  x@aliases <- value
+  validObject(x)
+  x
+})
+
+setGeneric("levelsType", function(x) standardGeneric("levelsType"))
+setGeneric("levelsType<-", function(x, value) standardGeneric("levelsType<-"))
+setMethod("levelsType","Column", function(x) x$levelsType)
+setMethod("levelsType<-","Column", function(x, value) {
+  x@levelsType <- value
+  validObject(x)
+  x
+})
+
+setGeneric("colLevels", function(x) standardGeneric("colLevels"))
+setGeneric("colLevels<-", function(x, value) standardGeneric("colLevels<-"))
+setMethod("colLevels","Column", function(x) x@colLevels)
+setMethod("colLevels<-","Column", function(x, value) {
+  x@colLevels <- value
+  validObject(x)
+  x
+})
+
+setGeneric("minLevel", function(x) standardGeneric("minLevel"))
+setGeneric("minLevel<-", function(x, value) standardGeneric("minLevel<-"))
+setMethod("minLevel","Column", function(x) x@minLevel)
+setMethod("minLevel<-","Column", function(x, value) {
+  x@minLevel <- value
+  validObject(x)
+  x
+})
+
+setGeneric("maxLevel", function(x) standardGeneric("maxLevel"))
+setGeneric("maxLevel<-", function(x, value) standardGeneric("maxLevel<-"))
+setMethod("maxLevel","Column", function(x) x@maxLevel)
+setMethod("maxLevel<-","Column", function(x, value) {
+  x@maxLevel <- value
+  validObject(x)
+  x
+})
+
+# Finally, we define both a dealias and a levelDealias method for a Column. "dealias" works
+# precisely as for a Level -- it looks at the name and aliases of the column, and sends any
+# input that matches the name or the aliases to the name, and any other input to NULL.
+# levelDealias runs its input against the dealias methods of the levels of the column, trying
+# to find if any succeed in dealiasing the input. If any did, it returns that success, otherwise
+# it returns NULL. If multiple succeed, it throws an error. (This should be prevented by the data
+# documentation validation if the column was constructed from XML documentation, however.)
+# levelDealias also works for NumericRange or Municipality columns, where it just checks that the
+# input has a valid value and then returns either the input or NULL depending on if it was valid
+# or not.
+# Both functions are of course vectorised.
+setMethod("dealias", "Column", function(x, toDealias) {
+  # We can actually reuse the code for dealiasing in a Level verbatim.
+  # Normally this might be cause to consider having both inherit from some shared
+  # parent class, but the way this might require multiple inheritance seems like
+  # more headache than it's worth to not repeat eight lines of code twice.
+  return(unname(unlist(sapply(toDealias, function(str) {
+    if (str == x@name) {
+      x@name
+    } else if (str %in% x@aliases) {
+      x@name
+    } else {
+      NULL
+    }
+  }))))
+})
+
+setGeneric("levelDealias", function(x,toDealias) standardGeneric("levelDealias"))
+setMethod("levelDealias", "Column", function(x, toDealias) {
+  # Again, we use a sapply with a unname(unlist()) wrapped around it, because that makes
+  # everything except that part much easier to read.
+  return(unname(unlist(sapply(toDealias, function(str) {
+    # We actually want to do slightly different things depending on the levelsType here.
+    # If it is of type Municipality, we return str if str is a municipality name, otherwise NULL:
+    if (x@levelsType == "Municipality") {
+      if (str %in% municipalityNames) {
+        str
+      } else {
+        NULL
+      }
+    }
+    # If it is of type NumericRange, we return NULL if str is not a number. If it is a number, and maxLevel
+    # or minLevel is set, we check if str is in the right range. If it is, we return it, otherwise NULL.
+    if (x@levelsType == "NumericRange") {
+      if (!is.numeric(str)) {
+        NULL
+      } else {
+        if (!is.na(x@maxLevel) && str > x@maxLevel) {
+          NULL
+        } else if (!is.na(x@minLevel) && str < x@minLevel) {
+          NULL
+        } else {
+          str
+        }
+      }
+    }
+    # If it is of type Character, we try to dealias str against every level, and check that this succeeds either
+    # zero times (in which case we return NULL) or once (in which case we return the successful value). If it succeeds
+    # more than once, we throw an error, since there is no unambiguous thing to dealias to.
+    if (x@levelsType == "Character") {
+      dealiasingResults <- unname(unlist(sapply(x@levels, function(lev) {
+        dealias(lev, str)
+      })))
+    }}}} # Intentional syntax error to remember where to start tomorrow morning! :D
     }
   }))))
 })
--- tests/testthat/test-genericTableClass.r
+++ tests/testthat/test-genericTableClass.r
@@ -33,3 +33,98 @@ test_that("Dealiasing of levels works", {
   expect_null(dealias(tl,"Not in the list"))
   expect_identical(dealias(tl,c("test","Test","Test","tset","NotInTheList")),c("Test","Test","Test","Test",NULL))
 })
+
+##############################################################################
+# Tests of the Column class:
+test_that("Column objects of levelsType Municipalities can be created manually",{
+  tc <- new("Column", name="region", aliases=c("Kommun", "Municipality"), levelsType="Municipalities")
+  expect_s4_class(tc, "Column")
+})
+test_that("Column objects of levelsType Municipalities can be created by constructor",{
+  tc <- Column(name="region", aliases=c("Kommun", "Municipality"), levelsType="Municipalities")
+  expect_s4_class(tc, "Column")
+})
+
+test_that("Column objects of levelsType NumericRange can be created manually", {
+  tc1 <- new("Column", name="alder", aliases=c("Age","age"), levelsType="NumericRange")
+  tc2 <- new("Column", name="alder", aliases=c("Age","age"), levelsType="NumericRange", maxLevel=23)
+  tc3 <- new("Column", name="alder", aliases=c("Age","age"), levelsType="NumericRange", maxLevel=45, minLevel=23)
+  expect_s4_class(tc1, "Column")
+  expect_s4_class(tc2, "Column")
+  expect_s4_class(tc3, "Column")
+})
+test_that("Column objects of levelsType NumericRange can be created by constructor", {
+  tc1 <- Column(name="alder", aliases=c("Age","age"), levelsType="NumericRange")
+  tc2 <- Column(name="alder", aliases=c("Age","age"), levelsType="NumericRange", maxLevel=23)
+  tc3 <- Column(name="alder", aliases=c("Age","age"), levelsType="NumericRange", maxLevel=45, minLevel=23)
+  expect_s4_class(tc1, "Column")
+  expect_s4_class(tc2, "Column")
+  expect_s4_class(tc3, "Column")
+})
+
+test_that("Column objects of levelsType Character can be created manually", {
+  tl <- Level("Test", c("Test","test","tset"))
+  tl2 <- Level(name = "Test", aliases = c("Test","test","tset"))
+  tc <- new("Column", name="Tests", aliases=c("test","testa"), levelsType="Character", colLevels=list(tl,tl2))
+  expect_s4_class(tc,"Column")
+})
+test_that("Column objects of levelsType Character can be created by constructor", {
+  tl <- Level("Test", c("Test","test","tset"))
+  tl2 <- Level(name = "Test", aliases = c("Test","test","tset"))
+  tc <- Column(name="Tests", aliases=c("test","testa"), levelsType="Character", colLevels=list(tl,tl2))
+  expect_s4_class(tc,"Column")
+})
+
+test_that("Validation of Column objects works", {
+  # Some argument is required:
+  expect_error(Column())
+  # Name should be a single string, not many:
+  expect_error(Column(name=c("One","Two"),aliases="na",levelsType = "Municipalities"))
+  # levelsType should be a single string, not many:
+  expect_error(Column(name="n",aliases="na",levelsType = c("One","Two")))
+  # levelsType should be one of NumericRange, Municipalities, or Character:
+  expect_error(Column(name="n", levelsType="Hiya!"))
+
+  # Supplying a maxLevel or minLevel to a Column where the levelsType is not NumericRange should cause a warning:
+  expect_warning(Column(name="", aliases="", levelsType = "Municipalities", maxLevel = 35))
+  # Supplying a colLevels to a column that does not have levelsType Character should cause a warning:
+  expect_warning(Column(name="", levelsType = "NumericRange", colLevels = list(1,2,3)))
+
+  # Supplying a colLevels that is not a list of valid level objects, or no colLevels at all, should cause an error:
+  expect_error(Column(name="", levelsType = "Character"))
+  expect_error(Column(name="", levelsType = "Character", colLevels = list(1,2,3)))
+  # Even if one of the levels is valid, we should fail if there is an invalid object:
+  tl <- Level("Test", c("Test","test","tset"))
+  tl_invalid <- Level("Test", c("Test","test","tset"))
+  tl_invalid@name <- c("Test","Best") # This creates an object of class Level that is not a *valid* object
+  expect_error(Column(name="", levelsType = "Character", colLevels = list(1,2,tl)))
+  expect_error(Column(name="", levelsType = "Character", colLevels = list(tl_invalid)))
+  expect_error(Column(name="", levelsType = "Character", colLevels = list(tl, tl_invalid)))
+})
+
+test_that("Setters and getters of Column object slots works", {
+  tc1 <- Column(name="alder", aliases=c("Age","age"), levelsType="NumericRange")
+  tc2 <- Column(name="alder", aliases=c("Age","age"), levelsType="NumericRange", maxLevel=23)
+
+  tl <- Level("Test", c("Test","test","tset"))
+  tl2 <- Level(name = "Test", aliases = c("Test","test","tset"))
+  tc3 <- new("Column", name="Tests", aliases=c("test","testa"), levelsType="Character", colLevels=list(tl,tl2))
+
+  expect_identical(name(tc1),"alder")
+  name(tc1) <- "NewName"
+  expect_identical(name(tc1),"NewName")
+  expect_error(name(tc1) <- c("Two", "Names"))
+
+  expect_identical(maxLevel(tc2),23)
+  maxLevel(tc2) <- 56
+  expect_identical(maxLevel(tc2), 56)
+
+  expect_true(is.na(minLevel(tc2)))
+  minLevel(tc2) <- 12
+  expect_identical(minLevel(tc2), 12)
+
+  expect_identical(colLevels(tc3), list(tl, tl2))
+  colLevels(tc3) <- list(tl)
+  expect_identical(colLevels(tc3), list(tl))
+  expect_error(colLevels(tc3) <- list(1,2,3))
+})
