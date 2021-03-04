574231dfeb981a4a00de6718f51b65fb62bc8918
vagdur <vilhelm.agdur@gmail.com>
2021-03-05 00:26:06 +0100

    Defined class DocumentedTable and wrote query functions
    The DocumentedTable class is the kind of object that we want to be generating from our XML documentation -- essentially a rather complicated wrapper around a CSV file, containing and enforcing some constraints on it.
    
    The query function on it is mostly just an overengineered version of subset, that can handle alternative names for columns and levels as documented in the XML. The magic will happen when we have a bunch of these DocumentedTables and have the remaining small piece of code to query all of them in a uniform way, sparing the user from having to think about which file which data point is in.
    
    Note that all this new code is so far completely untested. So the next step is obviously to write tests of all this, and catch and fix all the hidden bugs that are inevitably there in untested code.

--- R/genericTableClass.r
+++ R/genericTableClass.r
@@ -301,3 +301,295 @@ setMethod("levelDealias", "Column", function(x, toDealias) {
     }
   }))))
 })
+
+###################################################################################################################################
+# Definition of the DocumentedTable class
+# The name is chosen to specify what kind of table we're dealing with.
+# A DocumentedTable contains a bunch of things:
+#   1.  A name of the table
+#   2.  A description of the table
+#   3.  A source for the table
+#   4.  A year for when the data is from
+#   5.  A data.frame containing the actual data -- this should not be accessed or modified by the user.
+#   6.  A list of Column objects documenting the columns, each of which contains more information
+#   7.  A name of the column in the data.frame containing the numeric data
+#
+# There are a few methods that work on a DocumentedTable:
+#   1.  Getters and setters for the first four points on the list above
+#   2.  columnDealias: Implements dealiasing of columns. See the comment before that method for more detail.
+#   3.  query: Implements querying against the table. See the comment before that function, or the documentation of the library for the
+#       general format of querying.
+#   4.  canHandleQuery: Checks whether a query makes sense against the table, and returns true or false without actually executing the query.
+#       Used to route queries to the right table when we have a bunch of them. Same arguments as Query, of course.
+
+# We start by defining the class:
+setOldClass("data.frame") # Not sure if we really need this? Better safe than sorry.
+setClass("DocumentedTable",
+         slots = c(
+           name = "character",
+           description = "character",
+           dataSource = "character",
+           dataYear = "integer",
+           csvData = "data.frame",
+           tableColumns = "list",
+           valueColumn = "character"
+         ),
+         prototype = c(
+           name = NA_character_,
+           description = NA_character_,
+           dataSource = NA_character_,
+           dataYear = NA_integer_,
+           csvData = data.frame(),
+           tableColumns = list(),
+           valueColumn = NA_character_
+         ))
+# Define a constructor function:
+DocumentedTable <- function(name = NA_character_,
+                            description = NA_character_,
+                            dataSource = NA_character_,
+                            dataYear = NA_integer_,
+                            csvData = data.frame(),
+                            tableColumns = list(),
+                            valueColumn = NA_character_) {
+  new("DocumentedTable", name = name, description = description, dataSource = dataSource,
+      dataYear = dataYear, csvData = csvData, tableColumns = tableColumns, valueColumn = valueColumn)
+}
+# Define a validator:
+setValidity("DocumentedTable", function(object) {
+  # This does fewer tests than it perhaps strictly should, because for DocumentedTable objects constructed
+  # from an XML, all of these things and more are already tested by the test suite for the XML documentation.
+  # We restrict ourselves to a minimum of tests to make sure it isn't completely broken here.
+
+  # For the name, description, dataSource, and dataYear, there are no requirements other than their types,
+  # and that they be a single string, not multiple:
+  if (length(object@name) != 1) {
+    return("DocumentedTable should have one name, not zero or multiple.")
+  }
+  if (length(object@description) != 1) {
+    return("DocumentedTable should have one description, not zero or multiple.")
+  }
+  if (length(object@dataSource) != 1) {
+    return("DocumentedTable should have one data source, not zero or multiple.")
+  }
+  if (length(object@dataYear) != 1) {
+    return("DocumentedTable should have one data year, not zero or multiple.")
+  }
+  # There are however a few checks needed on the remaining slots. We begin by checking that tableColumns is
+  # indeed a list of valid Column objects, and the name of each occurs as a
+  for (tableColumn in object@tableColumns) {
+    if(!is(tableColumn) == "Column") {
+      return("One of the elements of tableColumns is not a Column object.")
+    }
+    validObject(tableColumn)
+    if(!(name(tableColumn) %in% colnames(object@csvData))) {
+      return("One of the elements of tableColumns has a name that does not occur in the column names of the CSV data.")
+    }
+  }
+  # Having checked that, we now check that valueColumn is indeed one of the column names of our data frame. First,
+  # however, we check that it is a single entry, not multiple:
+  if (length(object@valueColumn) != 1) {
+    return("DocumentedTable should have one valueColumn, not zero or multiple.")
+  }
+  if (!(object@valueColumn %in% colnames(object@csvData))) {
+    return("Specified valueColumn does not occur as a column of the csvData.")
+  }
+
+  # Everything was okay, so we return TRUE:
+  return(TRUE)
+})
+# Define setter and getter methods: (I hate how un-DRY this code is... Might be possible to just do this with some fancy meta-programming?)
+setMethod("name","DocumentedTable", function(x) { x@name })
+setMethod("name<-", "DocumentedTable", function(x, value) {
+  x@name <- value
+  validObject(x)
+  x
+})
+
+setGeneric("description", function(x) standardGeneric("description"))
+setGeneric("description<-", function(x, value) standardGeneric("description<-"))
+setMethod("description","DocumentedTable", function(x) x$description)
+setMethod("description<-","DocumentedTable", function(x, value) {
+  x@description <- value
+  validObject(x)
+  x
+})
+
+setGeneric("dataSource", function(x) standardGeneric("dataSource"))
+setGeneric("dataSource<-", function(x, value) standardGeneric("dataSource<-"))
+setMethod("dataSource","DocumentedTable", function(x) x$dataSource)
+setMethod("dataSource<-","DocumentedTable", function(x, value) {
+  x@dataSource <- value
+  validObject(x)
+  x
+})
+
+setGeneric("dataYear", function(x) standardGeneric("dataYear"))
+setGeneric("dataYear<-", function(x, value) standardGeneric("dataYear<-"))
+setMethod("dataYear","DocumentedTable", function(x) x$dataYear)
+setMethod("dataYear<-","DocumentedTable", function(x, value) {
+  x@dataYear <- value
+  validObject(x)
+  x
+})
+
+setGeneric("csvData", function(x) standardGeneric("csvData"))
+setGeneric("csvData<-", function(x, value) standardGeneric("csvData<-"))
+setMethod("csvData","DocumentedTable", function(x) x$csvData)
+setMethod("csvData<-","DocumentedTable", function(x, value) {
+  x@csvData <- value
+  validObject(x)
+  x
+})
+
+setGeneric("tableColumns", function(x) standardGeneric("tableColumns"))
+setGeneric("tableColumns<-", function(x, value) standardGeneric("tableColumns<-"))
+setMethod("tableColumns","DocumentedTable", function(x) x$tableColumns)
+setMethod("tableColumns<-","DocumentedTable", function(x, value) {
+  x@tableColumns <- value
+  validObject(x)
+  x
+})
+
+setGeneric("valueColumn", function(x) standardGeneric("valueColumn"))
+setGeneric("valueColumn<-", function(x, value) standardGeneric("valueColumn<-"))
+setMethod("valueColumn","DocumentedTable", function(x) x$valueColumn)
+setMethod("valueColumn<-","DocumentedTable", function(x, value) {
+  x@valueColumn <- value
+  validObject(x)
+  x
+})
+
+# Define columnDealias:
+setGeneric("columnDealias", function(x, toDealias) standardGeneric("columnDealias"))
+setMethod("columnDealias", "DocumentedTable", function(x, toDealias) {
+  # As before, we use this slight hack to easily vectorise the function, and the rest of the code
+  # can pretend like the arguments we got were x and str:
+  unname(unlist(sapply(toDealias, function(str) {
+    # This is essentially a copy-paste of the code for levelDealias -- perhaps another argument
+    # to try to code this with multiple-inheritance from a "dealiasable" class as well as the
+    # normal inheritance? Worth thinking about.
+
+    # We try to dealias str against every column, and then see if it succeeded zero times (in
+    # which case we return NULL), once (in which case we return the value of that success), or
+    # more than once, in which case we throw an error since we couldn't find an unambiguous
+    # result but really should be able to if the table is set up right.
+
+    dealiasingResults <- unname(unlist(sapply(x@tableColumns, function(col) {
+      dealias(col, str)
+    })))
+    if (is.null(dealiasingResults)) {
+      # So none of the columns could dealias it, so we return NULL:
+      NULL
+    } else if (length(dealiasingResults) == 1) {
+      # WARNING: For some reason, if we return dealiasingResults, it pops out as an n x 1 matrix, but inside the actual function,
+      # it is as expected just a vector. This is very surprising, but borne out by inserting a browser() right after the sapply.
+      # I wonder why R does that to the return value?
+      dealiasingResults[1]
+    } else if (length(dealiasingResults) > 1) {
+      # More than one column succeeded in dealiasing! This should not happen -- in a table constructed from an XML file
+      # that has been tested, this is impossible. We throw an error.
+      stop("More than one column succeeded in dealiasing. Are you sure your column has been constructed correctly?")
+    } else {
+      # This really should not be possible. Since there should be no way to reach this point, we throw an error here
+      # to be alerted if our assumptions that this is unreachable are violated:
+      stop("Reached case in conditional that should not be reachable. This should never run, so something weird has happened.")
+    }
+  })))
+})
+
+# Define canHandleQuery:
+setGeneric("canHandleQuery", function(x, ...) standardGeneric("canHandleQuery"))
+setMethod("canHandleQuery", "DocumentedTable", function(x, ...) {
+  # This function needs to do some odd things with ... -- at some point at least
+  # we will want to test that we are using all arguments of ... . There is an experimental
+  # package to do this.
+  #TODO: Implement this.
+
+  # First we pull out the names of the arguments we got: (Note that "list(...)" evaluates all the arguments. Right now
+  # we don't care about this, but this does prevent us from doing metaprogramming dark magics to the stuff in list(...))
+  argumentNames = names(list(...))
+  # Then we check that we can dealias each of these to a proper column name:
+  dealiasedArgumentNames <- columnDealias(x, argumentNames)
+  if (length(dealiasedArgumentNames) != length(argumentNames)) {
+    # At least one argument name failed to dealias, so we don't know what to do with this, and
+    # we return FALSE to indicate the table cannot handle this query:
+    return(FALSE)
+  }
+  # Assuming we got to this point, each argument name corresponds to a column we do have. Time to check that
+  # each argument value corresponds to a (potential) level of that column:
+  argumentValues <- list(...)
+  for (argumentName in argumentNames) {
+    #TODO: This is a really bad algorithm, since it double loops, once over argumentNames and once over the columns
+    # per argumentName. Should figure out a way to do this with a single loop? Might not be a huge issue as long as
+    # we only have like ten columns, though.
+    argumentValue <- argumentValues[[argumentName]]
+    for (column in x@tableColumns) {
+      # If this column is the one that the argument name refers to...
+      if (name(column) == columnDealias(x,argumentName)) {
+        # ... we try to dealias the value of the argument against this column...
+        dealiasedArgumentValue <- levelDealias(column, argumentValue)
+        if (length(dealiasedArgumentValue) != length(argumentValue)) {
+          # ... and if at least one of the elements of argumentValue failed to dealias against
+          # this column, we conclude that while the column name matches the parameter we were
+          # passed, we are nonetheless not in the right table, since an invalid level was asked for.
+          # Perhaps there is some other table with the same column name that can handle this? We,
+          # however, return FALSE:
+          return(FALSE)
+        }
+      }
+    }
+  }
+
+  return(TRUE)
+})
+
+# Define query:
+setGeneric("query", function(x, verbose, checkHandleable, failIfUnable, ...) standardGeneric("query"))
+setMethod("query", "DocumentedTable", function(x, verbose=FALSE, checkHandleable=TRUE, failIfUnable=FALSE, ...) {
+  # First, we check if we can even handle the query, in the case where checkHandleable is TRUE:
+  if (checkHandleable) {
+    if (!canHandleQuery(x, ...)) {
+      # So we can't, what do we do? We either throw an error or just a warning, depending on the failIfUnable parameter.
+      # In the latter case we return NA, to mark no data was found.
+      if (failIfUnable) {
+        stop("Unable to handle this query, either a column name or a level did not match what the table contains.")
+      } else {
+        warning("Unable to handle this query, either a column name or a level did not match what the table contains. Returning NA.")
+        return(NA_integer_)
+      }
+    }
+  }
+  # So we either checked handleability and passed, or we were told by the checkHandleable that this has already been checked before
+  # this function was called and we can safely steam on ahead.
+
+  # First thing we need to do is grab our parameters from ... and dealias them into something our raw CSV can handle:
+  argumentNames = names(list(...))
+  argumentValues <- list(...)
+
+  # Then, we just loop over all our parameters and subset down the raw csvData according to the specifications of the parameters.
+  #TODO: I strongly suspect this is a really really crappy algorithm. It should be replaced by something better, and perhaps
+  # more idiomatic in R as well.
+  runningSubset <- x@csvData
+  for (argumentName in argumentNames) {
+    # First we dealias the argumentName
+    dealiasedArgumentName <- columnDealias(x, argumentName)
+    # Then we loop over all the columns to find the right one:
+    #TODO: This is a dumb algorithm, even if the algorithm this is in is kept, this part should be replaced.
+    for (column in x@tableColumns) {
+      if (name(column) == dealiasedArgumentName) {
+        # So this is the column we were looking for!
+        # Now we can dealias also the argument values:
+        dealiasedArgumentValue <- levelDealias(column, argumentValues[[argumentName]])
+
+        # Okay, so now we have both the right name for the column and the right name for the levels,
+        # so now we can just do the subsetting:
+        desiredIndices <- (runningSubset[,dealiasedArgumentName] %in% dealiasedArgumentValue)
+        runningSubset <- runningSubset[desiredIndices,]
+      }
+    }
+  }
+
+  # So now we have the subset that filtered down to. This is the easiest step: We just sum over
+  # the valueColumn in this subset and return that number:
+  return(sum(runningSubset[,x@valueColumn]))
+})
